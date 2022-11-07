1726366088f545bb16f37342ae00e39fb96bc3be
Daniele <daniele.trapani96@gmail.com>
2022-11-07 19:41:55 +0100

    calculations added

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+previousValue = ''
+currentValue = ''
+currentOperator = ''
+
 function add(a, b) {
     return a + b;
 }
@@ -15,6 +19,8 @@ function divide(a, b) {
 }
 
 function operate(a, b, oper) {
+    a = a * 1
+    b = b * 1
     switch (oper) {
         case '+':
             result = add(a, b)
@@ -31,4 +37,42 @@ function operate(a, b, oper) {
     }
 }
 
-// console.log(operate(10, 5, '+'))
+const lastOperation = document.getElementById("lastOperation")
+const currentOperation = document.getElementById('currentOperation')
+const buttons = document.querySelectorAll('button')
+// buttons.forEach(button => button.addEventListener('click', () => console.log(button)))
+buttons.forEach(button => button.addEventListener('click', () => compute(button)))
+
+//on click if num add to current screen, if operator store operator, if equals computer
+function compute(button) {
+    // if (button.classList.contains('num')) {
+    //     console.log("hello")
+    // }
+    // console.log(button.classList[1])
+    switch (button.classList[1]) {
+        case 'num':
+            currentValue += button.textContent;
+            currentOperation.textContent = currentValue
+            break;
+        case 'point':
+            if (currentValue.includes('.')) {
+                break;
+            }
+            else {
+                currentValue += '.'
+                currentOperation.textContent = currentValue
+                break;
+            }
+        case 'operator':
+            currentOperator = button.textContent
+            lastOperation.textContent = currentValue + ' ' + currentOperator
+            currentOperation.textContent = ''
+            previousValue = currentValue;
+            currentValue = ''
+            break;
+        case 'equals':
+            result = operate(previousValue, currentValue, currentOperator)
+            lastOperation.textContent = ''
+            currentOperation.textContent = result
+    }
+}
